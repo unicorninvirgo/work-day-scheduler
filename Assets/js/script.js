95729ea5ef6f57d47e97b3f7357a6b0e03764714
Unicorn Virgo <58121705+unicorninvirgo@users.noreply.github.com>
2021-07-25 20:33:37 -0400

    added time of day color changes to calendar

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,8 +4,14 @@ var elCurrentDay = $('#currentDay');
 
 
 //capture and format today's date
-const today = moment().format("dddd, MMMM Do YYYY");
+var today = moment().format("dddd, MMMM Do YYYY");
+var currentHour = moment().format("MM-DD-YYYY hh:00 A");
+
 elCurrentDay.append(today);
+
+//var time = '8:00 PM'
+
+//var eventDateTime = moment(today + ' ' + time, "dddd, MMMM Do YYYY hh:00 A").format("MM-DD-YYYY hh:00 A") ;
 
 //log storage
 let eventStorage = localStorage.getItem("calendarEvents") || [];
@@ -14,6 +20,9 @@ let eventStorage = localStorage.getItem("calendarEvents") || [];
 for( i=0; i < 24; i++){
     let x = 0;
     let ap = '';
+    let colorClass = '';
+    var time;
+    var eventDateTime;
 
     //set midday
     i < 12 ? ap = 'AM' : ap = 'PM';
@@ -30,26 +39,75 @@ for( i=0; i < 24; i++){
         x = i;
     }
 
+    time = `${x}:00 ${ap}`
+    eventDateTime = moment(today + ' ' + time, "dddd, MMMM Do YYYY hh:00 A").format("MM-DD-YYYY hh:00 A") ;
+
+    
+    switch (checkDate(eventDateTime))
+    {
+        case "before": 
+            colorClass = 'past';
+            break;
+        case "after":
+            colorClass = 'future';
+        case "same":
+            colorClass = 'present';
+            break;
+
+    }
+
+    console.log(colorClass);
+
     //format div for calendar day row
     elTimeBlock.append(`
         <div class="row">
             <div class="col" data-time="${x}:00${ap}">
-                ${x}:00 ${ap}
+                ${time}
             </div>
-            <div class="col-8" data-event-time="${x}:00${ap}">
+            <div class="col-8 ${colorClass}" data-event-time="${x}:00${ap}">
                 <textarea></textarea>
             </div>
-            <div class="col event-save" data-event-button="">
+            <div class="col event-save" data-event-button="$ {x}:00${ap}">
                <p><i class="fas fa-book fa-2x"></i></p>
             </div>
         </div>`);
 }
 
+function functionFormatDate(){
+
+}
+
+function checkDate(eventDateTime){
+    var colorVal = '';
+
+   if(moment(eventDateTime,"MM-DD-YYYY hh:00 A").isBefore(moment(currentHour,"MM-DD-YYYY hh:00 A")))
+   {
+       colorVal = 'before';
+   }
+
+   if(moment(eventDateTime,"MM-DD-YYYY hh:00 A").isSame(moment(currentHour,"MM-DD-YYYY hh:00 A")))
+   {
+       colorVal = 'same';
+   }
+
+   if(moment(eventDateTime,"MM-DD-YYYY hh:00 A").isAfter(moment(currentHour,"MM-DD-YYYY hh:00 A")))
+   {
+       colorVal = 'after';
+   }
+
+   return colorVal;
+
+}
+
 //save user calendar events to storage
-function saveEvent(){
+function saveEvent(event){
+   
+   let enteredEvent = $(event.target).attr('data-letter');
+
+   console.log(enteredEvent);
 
     var userEvent = {
-        userDate: "",
+        userDate: today,
         userTime: "",
         userEvent: ""
     };
@@ -59,6 +117,4 @@ function saveEvent(){
   
 }
 //capture event to save event
-elTimeBlock.on("click",".event-save", function(event){
-    alert(event.target);
-})
+elTimeBlock.on("click",".event-save", saveEvent)
